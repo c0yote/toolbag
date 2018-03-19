a09a7ae9b60053c6ca47a124a4252822b3e018f7
U.G. Wilson <ugwilson@gmail.com>
2018-03-19 02:02:42 -0500

    Cleaning up hashing utility.

--- tests/test_hash.py
+++ tests/test_hash.py
@@ -10,24 +10,24 @@ TEST_DATA_MD5 = '1e50210a0202497fb79bc38b6ade6c34'
 TEST_DATA_SHA256 = '1307990e6ba5ca145eb35e99182a9bec46531bc54ddf656a602c780fa0240dee'
 TEST_FILE_PATH = '/path/to/file'
 
-class HashFileToHexStrInChunksFuncTestCase(unittest.TestCase):
+class HashFileInChunksToHexStr_Func_TestCase(unittest.TestCase):
   
   @patch('builtins.open', new_callable=mock_open, read_data=TEST_DATA_RAW)
   def test_returns_hash_strings(self, mock_file):
     hash_object = hashlib.md5()
     self.assertEqual(
-      hash.hash_file_to_hex_str_in_chunks(TEST_FILE_PATH, hash_object),
+      hash.hash_file_in_chunks_to_hex_str(TEST_FILE_PATH, hash_object),
       TEST_DATA_MD5)
       
     hash_object = hashlib.sha256()
     self.assertEqual(
-      hash.hash_file_to_hex_str_in_chunks(TEST_FILE_PATH, hash_object),
+      hash.hash_file_in_chunks_to_hex_str(TEST_FILE_PATH, hash_object),
       TEST_DATA_SHA256)
     
   @patch('builtins.open', new_callable=mock_open, read_data=TEST_DATA_RAW)
   def test_opens_file_rb_mode(self, mock_file):
     mock_hash_object = MagicMock()
-    hash.hash_file_to_hex_str_in_chunks(TEST_FILE_PATH, mock_hash_object)
+    hash.hash_file_in_chunks_to_hex_str(TEST_FILE_PATH, mock_hash_object)
     mock_file.assert_called_with(TEST_FILE_PATH, 'rb')
   
   def test_uses_default_buffer_size(self):
@@ -35,7 +35,7 @@ class HashFileToHexStrInChunksFuncTestCase(unittest.TestCase):
     with patch('builtins.open', mock_open(mock_file, read_data=TEST_DATA_RAW)) as the_mock_open:
     
       mock_hash_object = MagicMock()
-      hash.hash_file_to_hex_str_in_chunks(TEST_FILE_PATH, mock_hash_object)
+      hash.hash_file_in_chunks_to_hex_str(TEST_FILE_PATH, mock_hash_object)
       mock_file.read.assert_called_with(hash.DEFAULT_HASHING_BUFFER_SIZE)
       print('outside: '+str(mock_file))
     
--- toolbag/hash.py
+++ toolbag/hash.py
@@ -1,46 +1,62 @@
+import argparse
 import hashlib
 import math
+import sys
 
 DEFAULT_HASHING_BUFFER_SIZE = int(math.pow(2,16))
 
-def hash_file_to_hex_str_in_chunks(filename, hash_object, 
+def hash_file_in_chunks_to_hex_str(filename, hash_object, 
                         buffer_size=DEFAULT_HASHING_BUFFER_SIZE):
   with open(filename, 'rb') as file:
-    print('inside: '+str(file))
     chunk = file.read(buffer_size)
     while chunk:
       hash_object.update(chunk)
       chunk = file.read(buffer_size)
   return hash_object.hexdigest()
 
-def _get_parsed_arguments():
-  import argparse
-  parser = argparse.ArgumentParser(description='Compute the hash of a file.')
-  parser.add_argument('file', help='The file to hash.')
-  parser.add_argument('--md5', help='Compute an MD5 hash of the file.', 
-                      action="store_true")
-  parser.add_argument('--sha256', help='Compute an SHA-256 hash of the file.', 
-                      action="store_true")
-  return parser.parse_args()
+def _display_hash(label, hash):
+  print(f'{label:<12}{hash}')
   
-if __name__ == '__main__':
-  LEFT_COLUMN_WIDTH = 12
-
-  parsed_args = _get_parsed_arguments()
-  filename = parsed_args.file
-  compute_md5 = parsed_args.md5
-  compute_sha256 = parsed_args.sha256
+def _md5(filename):
+  hash_object = hashlib.md5()
+  hash_string = hash_file_in_chunks_to_hex_str(filename, hash_object)
+  _display_hash('MD5',hash_string)
+  
+def _sha256(filename):
+  hash_object = hashlib.sha256()
+  hash_string = hash_file_in_chunks_to_hex_str(filename, hash_object)
+  _display_hash('SHA-256',hash_string)
+  
+def main(args):
+  filename = args.file
+  compute_md5 = args.md5
+  compute_sha256 = args.sha256
   compute_all = not compute_md5 and not compute_sha256
   
   try:
     if compute_md5 or compute_all:
-      hash_object = hashlib.md5()
-      hash_string = hash_file_to_hex_str_in_chunks(filename, hash_object)
-      print('MD5: '.ljust(LEFT_COLUMN_WIDTH)+hash_string)
+      _md5(filename)
     if compute_sha256 or compute_all:
-      hash_object = hashlib.sha256()
-      hash_string = hash_file_to_hex_str_in_chunks(filename, hash_object)
-      print('SHA-256: '.ljust(LEFT_COLUMN_WIDTH)+hash_string)
+      _sha256(filename)
+      
   except FileNotFoundError:
-    print('Could not locate file: \''+parsed_args.file+'\'', file=sys.stderr)
+    print('Could not locate file: \''+args.file+'\'', file=sys.stderr)
+  
+def _build_argument_parser():
+  DESCRIPTION = 'Compute the hash of a file.'
+  ARG_FILE_HELP = 'The file to hash.'
+  ARG_MD5_HELP = 'Compute an MD5 hash of the file.'
+  ARG_SHA256_HELP = 'Compute an SHA-256 hash of the file.'
+  
+  parser = argparse.ArgumentParser(description=DESCRIPTION)
+  parser.add_argument('file', help=ARG_FILE_HELP)
+  parser.add_argument('--md5', help=ARG_MD5_HELP, action="store_true")
+  parser.add_argument('--sha256', help=ARG_SHA256_HELP, action="store_true")
+  parser.set_defaults(func=main)
+  return parser
+  
+if __name__ == '__main__':
+  arg_parser = _build_argument_parser()
+  args = arg_parser.parse_args()
+  args.func(args)
     

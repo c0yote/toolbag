1dff1cea14d0968745964f3f71a22b671e4d00e1
U.G. Wilson <ugwilson@gmail.com>
2018-03-21 23:36:48 -0500

    hash module cleanup and eased ability to add more hashes.

--- toolbag/hash.py
+++ toolbag/hash.py
@@ -15,7 +15,7 @@ def hash_file_in_chunks_to_hex_str(filename, hash_object,
       chunk = file.read(chunk_size)
   return hash_object.hexdigest()
 
-class FileHashRenderer:
+class FileHash:
   def __init__(self, hash_name, hash_object, filename):
     self.hash_name = hash_name
     self.hash_string = hash_file_in_chunks_to_hex_str(filename, hash_object)
@@ -25,37 +25,57 @@ class FileHashRenderer:
 
 _LABEL = 0
 _HASH_OBJECT_CONSTRUCTOR = 1
+# Adding a hash to this dict automatically implements complete support for it.
 _SUPPORTED_HASHES = {
   'md5': ('MD5', hashlib.md5),
+  'sha1': ('SHA1', hashlib.sha1),
   'sha256': ('SHA256', hashlib.sha256),
 }
 
-def _build_hash_renderers(filename, alg_args=None):
-  if alg_args:
-    algorithms = args.algs.split(',')
-  else:
-    algorithms = ['md5', 'sha256']
-  
-  renderers = list()
-  if 'md5' in algorithms:
-    renderers.append(FileHashRenderer('MD5', hashlib.md5(), filename))
-  if 'sha256' in algorithms:
-    renderers.append(FileHashRenderer('SHA256', hashlib.sha256(), filename))
+def _build_hashes(filename, selected_algorithms):
+  hashes = list()
+  for alg in selected_algorithms:
+    hash_alg = _SUPPORTED_HASHES[alg]
+    label = hash_alg[_LABEL]
+    hash_obj = hash_alg[_HASH_OBJECT_CONSTRUCTOR]()
+    
+    hashes.append(FileHash(label, hash_obj, filename))
 
-  return renderers
+  return hashes
   
 def _confirm_file_exists(filename):
   if not os.path.isfile(filename):
     sys.exit('Could not locate file: \''+filename+'\'')
-  
+
+def _get_all_supported_hashes():
+  return _SUPPORTED_HASHES.keys()
+    
+def _get_selected_algorithms(alg_args):
+  if alg_args:
+    selected_algorithms = args.algs.split(',')
+  else:
+    selected_algorithms = _get_all_supported_hashes()
+    
+  return selected_algorithms
+    
+def _get_supported_hashes_str():
+  hashes_list_str = ''
+  for k,v in _SUPPORTED_HASHES.items():
+    hashes_list_str += ' ' + k
+
+  return hashes_list_str
+    
 def _main(args):
   _confirm_file_exists(args.file)
   
-  for r in _build_hash_renderers(args.file, args.algs):
+  filename = args.file
+  selected_algs = _get_selected_algorithms(args.algs)
+  
+  for r in _build_hashes(filename, selected_algs):
     print(r)
   
 def _build_argument_parser():
-  DESCRIPTION = 'Compute the hash of a file.'
+  DESCRIPTION = 'Compute the hash of a file. Supports: '+_get_supported_hashes_str()
   ARG_FILE_HELP = 'The file to hash.'
   ARG_ALGS_HELP = 'choose hashing algorithms to use (ex. --algs=md5,sha256)'
   

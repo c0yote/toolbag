7cbe969bf13296eec72f4239a4d9468b9b76cb5c
U.G. Wilson <ugwilson@gmail.com>
2018-04-05 16:01:43 -0500

    Application and tests.

--- tests/test_logkeep.py
+++ tests/test_logkeep.py
@@ -1,19 +1,31 @@
+import os
 import unittest
+from unittest import skip
 from unittest.mock import MagicMock, mock_open, patch
 
-import os
+def fix_iteration_in_mock_open(open_mock):
+  """ A test utility function to help with file open mocking.
+  """
+  open_mock.return_value.__iter__ = lambda self : iter(self.readline, '')
+  
+from toolbag.logkeep import _Application, Log
 
-from toolbag.logkeep import Log
-
+BYTES_IN_MEGABYTE = 1000000
 TEST_LOG_PATH = '/some/path/to/log.log'
 TEST_LOG_DATA = f'line 0{os.linesep}line 1{os.linesep} line 2'
 TEST_LOG_DATA_SIZE = len(TEST_LOG_DATA.encode('utf-8'))
 TEST_LOG_DATA_LINE_CNT = 3
 TEST_USER_DEFINED_CLONE_PATH = '/user/path/to/log.bak'
-
-def fix_iteration_in_mock_open(open_mock):
-  open_mock.return_value.__iter__ = lambda self : iter(self.readline, '')
-
+TEST_LIMIT_VALUE_A = 10
+TEST_LIMIT_VALUE_B = 11
+TEST_LIMIT_GREATER = 12
+TEST_ARGS = ['app', TEST_LOG_PATH]
+TEST_SIZE_ARGS = ['app', '--size_limit', f'{TEST_LIMIT_VALUE_A}', TEST_LOG_PATH]
+TEST_LINE_ARGS = ['app', '--line_limit', f'{TEST_LIMIT_VALUE_B}', TEST_LOG_PATH]
+TEST_SIZE_AND_LINE_ARGS = ['app', '--size_limit', f'{TEST_LIMIT_VALUE_A}', 
+                            '--line_limit', f'{TEST_LIMIT_VALUE_B}',
+                            TEST_LOG_PATH]
+                            
 class Log_TestCase(unittest.TestCase):
   """ When we don't care about the mocks generated during patching, '*args' is 
       used in the test function parameters so there isn't a long list of useless 
@@ -23,14 +35,21 @@ class Log_TestCase(unittest.TestCase):
   def test_constructor_excepts_on_no_file(self):
     with self.assertRaises(FileNotFoundError):
       log = Log(TEST_LOG_PATH)
+
+  def test_constructor_excepts_when_path_is_not_a_file(self, *args):
+    with self.assertRaises(FileNotFoundError):
+      log = Log('./')
   
   @patch('os.path.getsize', return_value=TEST_LOG_DATA_SIZE)
+  @patch('toolbag.logkeep.Log._raise_if_not_a_file')
   @patch('builtins.open', new_callable=mock_open, read_data=TEST_LOG_DATA)
   def test_constructor_computes_size(self, *args):
     log = Log(TEST_LOG_PATH)
     self.assertEqual(TEST_LOG_DATA_SIZE, log.get_file_size_in_bytes())
-
+    self.assertEqual(TEST_LOG_DATA_SIZE/BYTES_IN_MEGABYTE, log.get_file_size_in_megabytes())
+    
   @patch('os.path.getsize', return_value=TEST_LOG_DATA_SIZE)
+  @patch('toolbag.logkeep.Log._raise_if_not_a_file')
   @patch('builtins.open', new_callable=mock_open, read_data=TEST_LOG_DATA)
   def test_constructor_counts_lines(self, open_, *args):
     fix_iteration_in_mock_open(open_)
@@ -47,6 +66,7 @@ class Log_TestCase(unittest.TestCase):
     
   @patch('os.path.getsize', return_value=TEST_LOG_DATA_SIZE)
   @patch('builtins.open', new_callable=mock_open, read_data=TEST_LOG_DATA)
+  @patch('toolbag.logkeep.Log._raise_if_not_a_file')
   @patch('shutil.copy2')
   def test_clone_to_a_backup_default(self, copy2_mock, *args):
     log = Log(TEST_LOG_PATH)
@@ -55,9 +75,97 @@ class Log_TestCase(unittest.TestCase):
 
   @patch('os.path.getsize', return_value=TEST_LOG_DATA_SIZE)
   @patch('builtins.open', new_callable=mock_open, read_data=TEST_LOG_DATA)
-  @patch('shutil.copy2') 
-  def test_clone_to_a_backup_wiht_user_defined_path(self, copy2_mock, *args):
+  @patch('toolbag.logkeep.Log._raise_if_not_a_file')
+  @patch('shutil.copy2')
+  def test_clone_to_a_backup_with_user_defined_path(self, copy2_mock, *args):
     log = Log(TEST_LOG_PATH)
     log.clone_to_a_backup(TEST_USER_DEFINED_CLONE_PATH)
     copy2_mock.assert_called_with(TEST_LOG_PATH, TEST_USER_DEFINED_CLONE_PATH)
     
+    
+    
+class Application_TestCase(unittest.TestCase):
+  @patch('sys.argv', TEST_SIZE_ARGS)
+  @patch('toolbag.logkeep.Log')
+  def test_size_threshold_storage(self, *args):
+    app = _Application()
+    self.assertEqual(app._size_limit_mb, TEST_LIMIT_VALUE_A)
+    self.assertEqual(app._line_limit, None)
+    
+  @patch('sys.argv', TEST_LINE_ARGS)
+  @patch('toolbag.logkeep.Log')
+  def test_line_threshold_storage(self, *args):
+    app = _Application()
+    self.assertEqual(app._size_limit_mb, None)
+    self.assertEqual(app._line_limit, TEST_LIMIT_VALUE_B)
+  
+  @patch('sys.argv', TEST_SIZE_AND_LINE_ARGS)
+  @patch('toolbag.logkeep.Log')
+  def test_size_and_line_threshold_storage(self, *args):
+    app = _Application()
+    self.assertEqual(app._size_limit_mb, TEST_LIMIT_VALUE_A)
+    self.assertEqual(app._line_limit, TEST_LIMIT_VALUE_B)
+    
+  @patch('sys.argv', TEST_ARGS)
+  @patch('toolbag.logkeep.Log')
+  def test_no_threshold_storage(self, *args):
+    app = _Application()
+    self.assertEqual(app._size_limit_mb, None)
+    self.assertEqual(app._line_limit, None)
+
+  @patch('sys.argv', TEST_ARGS)
+  @patch('toolbag.logkeep.Log')  
+  def test_work_log_creation(self, log_mock):
+    app = _Application()
+    log_mock.assert_called_with(TEST_LOG_PATH)
+
+  @patch('sys.argv', TEST_ARGS)
+  @patch('toolbag.logkeep.Log')  
+  def test_work_log_storage(self, log_mock):
+    app = _Application()
+    handle = log_mock()
+    self.assertEqual(app._work_log, handle)
+
+  @patch('sys.argv', TEST_ARGS)
+  @patch('toolbag.logkeep.Log')
+  def test_does_not_clone_log_on_no_thresholds(self, log_mock):
+    app = _Application()
+    app.run()
+    
+    log_mock.clone_to_a_backup.assert_not_called()
+
+  @patch('sys.argv', TEST_SIZE_ARGS)
+  @patch('toolbag.logkeep.Log')
+  def test_clones_log_on_size_threshold(self, log_mock):
+    handle = log_mock()
+    handle.get_file_size_in_megabytes.return_value = TEST_LIMIT_GREATER
+  
+    app = _Application()
+    app.run()
+    
+    handle.clone_to_a_backup.assert_called_with()
+    
+  @patch('sys.argv', TEST_LINE_ARGS)
+  @patch('toolbag.logkeep.Log')
+  def test_clones_log_on_line_count_threshold(self, log_mock):
+    handle = log_mock()
+    handle.get_line_count.return_value = TEST_LIMIT_GREATER
+  
+    app = _Application()
+    app.run()
+    
+    handle.clone_to_a_backup.assert_called_with()
+    
+  @patch('sys.argv', TEST_SIZE_AND_LINE_ARGS)
+  @patch('toolbag.logkeep.Log')
+  def test_clones_log_on_sizeand_line_count_threshold(self, log_mock):
+    handle = log_mock()
+    handle.get_line_count.return_value = TEST_LIMIT_GREATER
+  
+    app = _Application()
+    app.run()
+    
+    handle.clone_to_a_backup.assert_called_with()
+
+  #def test_error_on_no_permissions(self):
+    #pass
--- toolbag/logkeep.py
+++ toolbag/logkeep.py
@@ -1,16 +1,23 @@
+import argparse
 import os.path
 import shutil
 
 class Log:
+  BYTES_IN_MEGABYTE = 1000000
   DEFAULT_CLONE_EXTENSION = '.prev'
   
   def __init__(self, file_path):
+    Log._raise_if_not_a_file(file_path)
+    
     self._size = os.path.getsize(file_path)
     self._line_count = Log._count_lines_in_file(file_path)
     self._file_path = file_path
     
   def get_file_size_in_bytes(self):
     return self._size
+    
+  def get_file_size_in_megabytes(self):
+    return float(self._size / Log.BYTES_IN_MEGABYTE)
   
   def get_line_count(self):
     return self._line_count
@@ -30,4 +37,58 @@ class Log:
         line_count += 1
         
     return line_count
+
+  @staticmethod
+  def _raise_if_not_a_file(file_path):
+    if not os.path.isfile(file_path):
+      raise FileNotFoundError(f'No file found at \'{file_path}\'.')
     
+class _Application:
+  DESCRIPTION = ('Checks user defined thresholds (such as size or line count) '
+                 'to decide when to create backups and clear out logs.\n\n'
+                 'The application will check the defined theshold, copy the '
+                 'existing log to a backup, and then clear out the primary '
+                 'log.  Existing backups will be overwritten with the latest.')
+                 
+  def __init__(self):
+    arg_parser = _Application._build_arg_parser()
+    args = arg_parser.parse_args()
+    
+    self._line_limit = args.line_limit
+    self._size_limit_mb = args.size_limit
+    self._work_log = Log(args.log_file)
+
+  def run(self):
+    if self._is_past_line_threshold() or self._is_past_size_threshold():
+      self._work_log.clone_to_a_backup()
+      self._work_log.clear_original_file()
+
+  def _is_past_line_threshold(self):
+    if self._line_limit is not None:
+      return self._work_log.get_line_count() > self._line_limit
+    else:
+      return False
+  
+  def _is_past_size_threshold(self):  
+    if self._size_limit_mb is not None:
+      return self._work_log.get_file_size_in_megabytes() > self._size_limit_mb
+    else:
+      return False
+
+  @staticmethod
+  def _build_arg_parser():
+    ARG_FILE_HELP = 'The target log file.'
+    ARG_LINE_HELP = 'The maximum number of lines the log may have before backup.'
+    ARG_SIZE_HELP = 'The maximum size (MB) the log may be before backup.'
+    
+    parser = argparse.ArgumentParser(description=_Application.DESCRIPTION)
+    parser.add_argument('--size_limit', help=ARG_LINE_HELP, type=int)
+    parser.add_argument('--line_limit', help=ARG_SIZE_HELP, type=int)
+    parser.add_argument('log_file', help=ARG_FILE_HELP)
+    return parser
+      
+    
+if __name__ == '__main__':
+  app = _Application()
+  app.run()  
+  
